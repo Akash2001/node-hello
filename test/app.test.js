a34e26d2ec56ee5b3a648ab8374fd3981ca9daa4
Akash Gadhave <akashngadhave@gmail.com>
2026-06-17 22:26:08 +0530

    Added simple backend project.

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const request = require('supertest');
+
+const app = require('../app');
+
+test('GET / returns Hello World!', async () => {
+  const response = await request(app).get('/');
+
+  assert.equal(response.status, 200);
+  assert.equal(response.text, 'Hello World!');
+});
